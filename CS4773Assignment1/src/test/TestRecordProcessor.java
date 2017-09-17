e8bef287ce0290e992853e487c9e215d3a02bb6d
LungJoe <lungjoe107@gmail.com>
2017-09-17 14:54:13 -0500

    test file changed

--- CS4773Assignment1/src/test/TestRecordProcessor.java
+++ CS4773Assignment1/src/test/TestRecordProcessor.java
@@ -10,12 +10,12 @@ import misc.RecordProcessorEdited;
 
 public class TestRecordProcessor {
 	private static String expectedFromData1;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		StringBuffer fileContents = new StringBuffer();
 		Scanner fileInput = new Scanner(new File("expected1.txt"));
-		while(fileInput.hasNextLine())
+		while (fileInput.hasNextLine())
 			fileContents.append(fileInput.nextLine() + "\n");
 		expectedFromData1 = fileContents.toString();
 		fileInput.close();
@@ -23,16 +23,16 @@ public class TestRecordProcessor {
 
 	@Test
 	public void testFileData1() {
-		assertEquals(expectedFromData1, RecordProcessorEdited.processFile("data1.txt"));
+		assertEquals(expectedFromData1, RecordProcessorEdited.printEmployeeRecords("data1.txt"));
 	}
 
-	@Test
+	@Test(expected = RuntimeException.class)
 	public void testFileData2() {
-		assertEquals(null, RecordProcessorEdited.processFile("data2.txt"));
+		assertEquals("File is empty", RecordProcessorEdited.printEmployeeRecords("data2.txt"));
 	}
 
-	@Test (expected = NumberFormatException.class)
+	@Test(expected = NumberFormatException.class)
 	public void testFileData3() {
-		assertEquals(null, RecordProcessorEdited.processFile("data3.txt"));
+		assertEquals(null, RecordProcessorEdited.printEmployeeRecords("data3.txt"));
 	}
 }
